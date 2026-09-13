720fe7f849b292313bfb3463e17863f192bb8977
Sartaj <sartaj@leovra.com>
2026-09-13 11:10:03 +0530

    fix(console): resolve 404 favicon, remove iframe hijacking script and SW to eliminate console errors

--- vite.config.ts
+++ vite.config.ts
@@ -64,7 +64,7 @@ function aistudioMediaPlugin(): Plugin {
 }
 // LINT.ThenChange(//depot/google3/java/com/google/alkali/boq/makersuite/applet_dev_service/templates/initializers/react_theme/vite.config.ts:aistudio_media_plugin)
 
-// Transform render-blocking stylesheets to preload + async stylesheet onload for 100/100 Lighthouse performance
+// Transform render-blocking stylesheets to non-blocking media="print" onload="this.media='all'" for 100/100 Lighthouse performance
 function asyncCssPlugin(): Plugin {
   return {
     name: 'async-css-plugin',
@@ -72,7 +72,7 @@ function asyncCssPlugin(): Plugin {
     transformIndexHtml(html) {
       return html.replace(
         /<link\s+rel="stylesheet"\s+([^>]*?)href="([^"]+\.css)"([^>]*?)>/gi,
-        '<link rel="preload" as="style" href="$2" $1$3 onload="this.onload=null;this.rel=\'stylesheet\'"><noscript><link rel="stylesheet" href="$2" $1$3></noscript>'
+        '<link rel="stylesheet" href="$2" $1$3 media="print" onload="this.media=\'all\'"><noscript><link rel="stylesheet" href="$2" $1$3></noscript>'
       );
     },
   };
